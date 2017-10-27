4b33cc37d69029d8e5bfa65f6465cdbc8e130d8b
Tyler Conlee <tylerlconlee@gmail.com>
2017-10-27 14:45:00 -0700

    sorted out tickets with slas into their own struct

--- zendesk/api.go
+++ zendesk/api.go
@@ -13,65 +13,67 @@ import (
 var Log = logging.MustGetLogger("zendesk")
 
 type ZenOutput struct {
-	Tickets []struct {
-		URL        string      `json:"url"`
-		ID         int         `json:"id"`
-		ExternalID interface{} `json:"external_id"`
-		Via        struct {
-			Channel string `json:"channel"`
-			Source  struct {
-				From struct {
-				} `json:"from"`
-				To struct {
-				} `json:"to"`
-				Rel string `json:"rel"`
-			} `json:"source"`
-		} `json:"via"`
-		CreatedAt       time.Time     `json:"created_at"`
-		UpdatedAt       time.Time     `json:"updated_at"`
-		Type            interface{}   `json:"type"`
-		Subject         string        `json:"subject"`
-		RawSubject      string        `json:"raw_subject"`
-		Description     string        `json:"description"`
-		Priority        interface{}   `json:"priority"`
-		Status          string        `json:"status"`
-		Recipient       interface{}   `json:"recipient"`
-		RequesterID     int64         `json:"requester_id"`
-		SubmitterID     int64         `json:"submitter_id"`
-		AssigneeID      interface{}   `json:"assignee_id"`
-		OrganizationID  int64         `json:"organization_id"`
-		GroupID         int           `json:"group_id"`
-		CollaboratorIds []interface{} `json:"collaborator_ids"`
-		FollowerIds     []interface{} `json:"follower_ids"`
-		ForumTopicID    interface{}   `json:"forum_topic_id"`
-		ProblemID       interface{}   `json:"problem_id"`
-		HasIncidents    bool          `json:"has_incidents"`
-		IsPublic        bool          `json:"is_public"`
-		DueAt           interface{}   `json:"due_at"`
-		Tags            []string      `json:"tags"`
-		CustomFields    []struct {
-			ID    int         `json:"id"`
-			Value interface{} `json:"value"`
-		} `json:"custom_fields"`
-		SatisfactionRating struct {
-			Score string `json:"score"`
-		} `json:"satisfaction_rating"`
-		SharingAgreementIds []interface{} `json:"sharing_agreement_ids"`
-		Fields              []struct {
-			ID    int         `json:"id"`
-			Value interface{} `json:"value"`
-		} `json:"fields"`
-		TicketFormID            int         `json:"ticket_form_id"`
-		BrandID                 int         `json:"brand_id"`
-		SatisfactionProbability interface{} `json:"satisfaction_probability"`
-		Slas                    struct {
-			PolicyMetrics []interface{} `json:"policy_metrics"`
-		} `json:"slas"`
-		AllowChannelback bool `json:"allow_channelback"`
-	} `json:"tickets"`
+	Tickets      `json:"tickets"`
 	NextPage     interface{} `json:"next_page"`
 	PreviousPage interface{} `json:"previous_page"`
 	Count        int         `json:"count"`
+}
+
+type Tickets []struct {
+	URL        string      `json:"url"`
+	ID         int         `json:"id"`
+	ExternalID interface{} `json:"external_id"`
+	Via        struct {
+		Channel string `json:"channel"`
+		Source  struct {
+			From struct {
+			} `json:"from"`
+			To struct {
+			} `json:"to"`
+			Rel string `json:"rel"`
+		} `json:"source"`
+	} `json:"via"`
+	CreatedAt       time.Time     `json:"created_at"`
+	UpdatedAt       time.Time     `json:"updated_at"`
+	Type            interface{}   `json:"type"`
+	Subject         string        `json:"subject"`
+	RawSubject      string        `json:"raw_subject"`
+	Description     string        `json:"description"`
+	Priority        interface{}   `json:"priority"`
+	Status          string        `json:"status"`
+	Recipient       interface{}   `json:"recipient"`
+	RequesterID     int64         `json:"requester_id"`
+	SubmitterID     int64         `json:"submitter_id"`
+	AssigneeID      interface{}   `json:"assignee_id"`
+	OrganizationID  int64         `json:"organization_id"`
+	GroupID         int           `json:"group_id"`
+	CollaboratorIds []interface{} `json:"collaborator_ids"`
+	FollowerIds     []interface{} `json:"follower_ids"`
+	ForumTopicID    interface{}   `json:"forum_topic_id"`
+	ProblemID       interface{}   `json:"problem_id"`
+	HasIncidents    bool          `json:"has_incidents"`
+	IsPublic        bool          `json:"is_public"`
+	DueAt           interface{}   `json:"due_at"`
+	Tags            []string      `json:"tags"`
+	CustomFields    []struct {
+		ID    int         `json:"id"`
+		Value interface{} `json:"value"`
+	} `json:"custom_fields"`
+	SatisfactionRating struct {
+		Score string `json:"score"`
+	} `json:"satisfaction_rating"`
+	SharingAgreementIds []interface{} `json:"sharing_agreement_ids"`
+	Fields              []struct {
+		ID    int         `json:"id"`
+		Value interface{} `json:"value"`
+	} `json:"fields"`
+	TicketFormID            int         `json:"ticket_form_id"`
+	BrandID                 int         `json:"brand_id"`
+	SatisfactionProbability interface{} `json:"satisfaction_probability"`
+	Slas                    struct {
+		PolicyMetrics []interface{} `json:"policy_metrics"`
+	} `json:"slas"`
+	AllowChannelback bool `json:"allow_channelback"`
 }
 
 // GetAllTickets grabs the latest tickets from Zendesk and returns the JSON
@@ -90,6 +92,8 @@ func GetAllTickets(user string, key string, url string) (tickets ZenOutput) {
 	return tickets
 }
 
+// makeRequests takes the Zendesk auth information and sends the curl request
+// to Zendesk and returns a JSON blob
 func makeRequest(user string, key string, url string) (responseData []byte) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
@@ -112,6 +116,8 @@ func makeRequest(user string, key string, url string) (responseData []byte) {
 	return responseData
 }
 
+// parseJSON takes the JSON from makeRequest and unmarshals it into the
+// ZenOutput struct, allowing the data to be accessed
 func parseJSON(data []byte) (output ZenOutput) {
 	// Read response from HTTP client
 	bytes := json.RawMessage(data)
--- zendesk/sla.go
+++ zendesk/sla.go
@@ -9,12 +9,18 @@ func CheckSLA() {
 	config := c.LoadConfig()
 	tickets := GetAllTickets(config.Zendesk.User, config.Zendesk.APIKey, config.Zendesk.URL)
 
+	var sla Tickets
 	for _, ticket := range tickets.Tickets {
 		priority := getPriorityLevel(ticket.Tags)
 
 		Log.Debug("ID:", ticket.ID, ", Title:", ticket.Subject, ", SLA:", ticket.Slas, ", Tags:", ticket.Tags, ", Priority:", priority)
+		if priority > 0 {
+			sla = append(sla, ticket)
+		}
 
 	}
+
+	Log.Debug(sla)
 }
 
 // getPriorityLevel takes an individual ticket row from the Zendesk output and // returns a string of what priority level the ticket is tagged with
