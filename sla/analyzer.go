68bb3d65cf40357667f3586a7447c324b9175073
Tyler Conlee <tylerlconlee@gmail.com>
2017-11-02 09:11:35 -0700

    added comments

--- sla/analyzer.go
+++ sla/analyzer.go
@@ -13,9 +13,15 @@ import (
 var c config.Config
 var log = logging.MustGetLogger("sla")
 
+// InitSLA loads the configuration and any other package-wide variables
+// needed for processing SLAs
 func InitSLA() {
 	c = config.LoadConfig()
 }
+
+// GetTimer takes an instance of a ticket and determines the SLA timer that it // uses. For example, if a ticket has a tag that matches the tag for LevelOne,
+// and it has a priority of 'high', it will use config.SLA.LevelOne.High as the
+// timer.
 func GetTimer(ticket zendesk.ActiveTicket) (breach time.Duration) {
 	r := reflect.ValueOf(c.SLA)
 	f := reflect.Indirect(r).FieldByName(ticket.Level)
@@ -25,6 +31,8 @@ func GetTimer(ticket zendesk.ActiveTicket) (breach time.Duration) {
 	return time.Duration(str.Duration)
 }
 
+// GetBreach takes an instance of a ticket and returns the value of the next SLA
+// breach.
 func GetBreach(ticket zendesk.ActiveTicket) {
 	p := ticket.SLA[0].(map[string]interface{})
 	log.Debug(p["breach_at"])
