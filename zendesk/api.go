d21298f99efc651f6507ee2a28a247e1550ac17f
Tyler Conlee <tylerlconlee@gmail.com>
2018-02-13 14:36:30 -0800

    added top level user struct

--- zendesk/api.go
+++ zendesk/api.go
@@ -86,45 +86,47 @@ type Tickets []struct {
 }
 
 type User struct {
-	ID                   int64         `json:"id"`
-	URL                  string        `json:"url"`
-	Name                 string        `json:"name"`
-	Email                string        `json:"email"`
-	CreatedAt            time.Time     `json:"created_at"`
-	UpdatedAt            time.Time     `json:"updated_at"`
-	TimeZone             string        `json:"time_zone"`
-	Phone                interface{}   `json:"phone"`
-	SharedPhoneNumber    interface{}   `json:"shared_phone_number"`
-	Photo                interface{}   `json:"photo"`
-	LocaleID             int           `json:"locale_id"`
-	Locale               string        `json:"locale"`
-	OrganizationID       int64         `json:"organization_id"`
-	Role                 string        `json:"role"`
-	Verified             bool          `json:"verified"`
-	ExternalID           interface{}   `json:"external_id"`
-	Tags                 []interface{} `json:"tags"`
-	Alias                interface{}   `json:"alias"`
-	Active               bool          `json:"active"`
-	Shared               bool          `json:"shared"`
-	SharedAgent          bool          `json:"shared_agent"`
-	LastLoginAt          time.Time     `json:"last_login_at"`
-	TwoFactorAuthEnabled bool          `json:"two_factor_auth_enabled"`
-	Signature            interface{}   `json:"signature"`
-	Details              interface{}   `json:"details"`
-	Notes                interface{}   `json:"notes"`
-	RoleType             interface{}   `json:"role_type"`
-	CustomRoleID         interface{}   `json:"custom_role_id"`
-	Moderator            bool          `json:"moderator"`
-	TicketRestriction    string        `json:"ticket_restriction"`
-	OnlyPrivateComments  bool          `json:"only_private_comments"`
-	RestrictedAgent      bool          `json:"restricted_agent"`
-	Suspended            bool          `json:"suspended"`
-	ChatOnly             bool          `json:"chat_only"`
-	DefaultGroupID       interface{}   `json:"default_group_id"`
-	UserFields           struct {
-		Mrr                      int         `json:"mrr"`
-		SystemEmbeddableLastSeen interface{} `json:"system::embeddable_last_seen"`
-	} `json:"user_fields"`
+	User struct {
+		ID                   int64         `json:"id"`
+		URL                  string        `json:"url"`
+		Name                 string        `json:"name"`
+		Email                string        `json:"email"`
+		CreatedAt            time.Time     `json:"created_at"`
+		UpdatedAt            time.Time     `json:"updated_at"`
+		TimeZone             string        `json:"time_zone"`
+		Phone                interface{}   `json:"phone"`
+		SharedPhoneNumber    interface{}   `json:"shared_phone_number"`
+		Photo                interface{}   `json:"photo"`
+		LocaleID             int           `json:"locale_id"`
+		Locale               string        `json:"locale"`
+		OrganizationID       int64         `json:"organization_id"`
+		Role                 string        `json:"role"`
+		Verified             bool          `json:"verified"`
+		ExternalID           interface{}   `json:"external_id"`
+		Tags                 []interface{} `json:"tags"`
+		Alias                interface{}   `json:"alias"`
+		Active               bool          `json:"active"`
+		Shared               bool          `json:"shared"`
+		SharedAgent          bool          `json:"shared_agent"`
+		LastLoginAt          time.Time     `json:"last_login_at"`
+		TwoFactorAuthEnabled bool          `json:"two_factor_auth_enabled"`
+		Signature            interface{}   `json:"signature"`
+		Details              interface{}   `json:"details"`
+		Notes                interface{}   `json:"notes"`
+		RoleType             interface{}   `json:"role_type"`
+		CustomRoleID         interface{}   `json:"custom_role_id"`
+		Moderator            bool          `json:"moderator"`
+		TicketRestriction    string        `json:"ticket_restriction"`
+		OnlyPrivateComments  bool          `json:"only_private_comments"`
+		RestrictedAgent      bool          `json:"restricted_agent"`
+		Suspended            bool          `json:"suspended"`
+		ChatOnly             bool          `json:"chat_only"`
+		DefaultGroupID       interface{}   `json:"default_group_id"`
+		UserFields           struct {
+			Mrr                      int         `json:"mrr"`
+			SystemEmbeddableLastSeen interface{} `json:"system::embeddable_last_seen"`
+		} `json:"user_fields"`
+	} `json:"user"`
 }
 
 // GetAllTickets grabs the latest tickets from Zendesk and returns the JSON
