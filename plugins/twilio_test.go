8e199ec2a2748ef7c9b1507360fec37b24c63ec5
Tyler Conlee <tylerlconlee@gmail.com>
2018-06-13 14:34:15 -0700

    unset twilio phone number

--- plugins/twilio_test.go
+++ plugins/twilio_test.go
@@ -112,7 +112,40 @@ func TestTwilioSet(t *testing.T) {
 			if gotAttachment := TwilioSet(tt.args.n); !reflect.DeepEqual(gotAttachment, tt.wantAttachment) {
 				t.Errorf("TwilioSet() = %v, want %v", gotAttachment, tt.wantAttachment)
 			}
-			if TwilioPhone != tt.args.n {
+			if TwilioPhone != tt.wantTwilioPhone {
+				t.Errorf("TwilioSet() = %v, want %v", TwilioPhone, tt.wantTwilioPhone)
+			}
+		})
+	}
+}
+
+func TestTwilioUnset(t *testing.T) {
+	tests := []struct {
+		name            string
+		wantTwilioPhone string
+		wantAttachment  slack.Attachment
+	}{
+		{
+			name:            "Test for Unsetting Twilio Phone Number",
+			wantTwilioPhone: "",
+			wantAttachment: slack.Attachment{
+				Title: "Twilio 'To' Phone Number Unset",
+				Fields: []slack.AttachmentField{
+					slack.AttachmentField{
+						Title: "Current Phone Number",
+						Value: "",
+						Short: true,
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotAttachment := TwilioUnset(); !reflect.DeepEqual(gotAttachment, tt.wantAttachment) {
+				t.Errorf("TwilioUnset() = %v, want %v", gotAttachment, tt.wantAttachment)
+			}
+			if TwilioPhone != tt.wantTwilioPhone {
 				t.Errorf("TwilioSet() = %v, want %v", TwilioPhone, tt.wantTwilioPhone)
 			}
 		})
