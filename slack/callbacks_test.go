60aba3355519950b43b007d940d8ad6dc46631dd
Tyler Conlee <tylerlconlee@gmail.com>
2018-06-26 07:22:22 -0700

    added satisfaction decode tests

--- slack/callbacks_test.go
+++ slack/callbacks_test.go
@@ -1,6 +1,8 @@
 package slack
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_statusDecode(t *testing.T) {
 	type args struct {
@@ -51,6 +53,46 @@ func Test_statusDecode(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if gotImg := statusDecode(tt.args.status); gotImg != tt.wantImg {
 				t.Errorf("statusDecode() = %v, want %v", gotImg, tt.wantImg)
+			}
+		})
+	}
+}
+
+func Test_satisfactionDecode(t *testing.T) {
+	type args struct {
+		sat string
+	}
+	tests := []struct {
+		name  string
+		args  args
+		wantS string
+	}{
+		{
+			name: "Good satisfaction decode",
+			args: args{
+				sat: "good",
+			},
+			wantS: ":white_check_mark:",
+		},
+		{
+			name: "Bad satisfaction decode",
+			args: args{
+				sat: "bad",
+			},
+			wantS: ":x:",
+		},
+		{
+			name: "Unoffered satisfaction decode",
+			args: args{
+				sat: "unoffered",
+			},
+			wantS: ":heavy_minus_sign:",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotS := satisfactionDecode(tt.args.sat); gotS != tt.wantS {
+				t.Errorf("satisfactionDecode() = %v, want %v", gotS, tt.wantS)
 			}
 		})
 	}
