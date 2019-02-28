487b7b74e80455312f0a03ae01f17ac1060d8273
Tyler Conlee <tylerlconlee@gmail.com>
2019-02-28 11:48:02 -0800

    add loadtags command

--- datastore/tags.go
+++ datastore/tags.go
@@ -38,3 +38,48 @@ func SaveNewTag(data map[string]string) error {
 	err := db.QueryRow("INSERT INTO tags(tag, userid, channel, notify_type, created_at) VALUES ($1, $2, $3, $4, $5) RETURNING id", data["tag"], data["user"], data["channel"], data["notify_type"], time.Now()).Scan(&id)
 	return err
 }
+
+// LoadTags retrieves all of the tags from the database, either to list
+// or to loop through
+func LoadTags() (tags []map[string]interface{}) {
+	log.Info("Requesting tags from database", map[string]interface{}{
+		"module": "datastore",
+	})
+	rows, err := db.Query("SELECT id, tag, user, channel, notify_type, created_at, updated_at FROM tags WHERE id > 0")
+	if err != nil {
+		log.Error("Error grabbing database output for tags", map[string]interface{}{
+			"module": "datastore",
+			"error":  err,
+		})
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var (
+			id         int64
+			tag        string
+			user       string
+			channel    string
+			notifyType string
+			createdAt  string
+			updatedAt  string
+		)
+
+		if err := rows.Scan(&id, &tag, &user, &channel, &notifyType, &createdAt, &updatedAt); err != nil {
+			log.Error("Error parsing database output for tags", map[string]interface{}{
+				"module": "datastore",
+				"error":  err,
+			})
+		}
+
+		tags = append(tags, map[string]interface{}{
+			"id":          id,
+			"tag":         user,
+			"channel":     channel,
+			"notify_type": notifyType,
+			"created_at":  createdAt,
+			"updated_at":  updatedAt,
+		})
+	}
+	return tags
+}
